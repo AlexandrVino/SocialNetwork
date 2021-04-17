f34be2c996df67d1a9d553d262b6727de6a49178
ВундерВафля <64733647+AlexandrVino@users.noreply.github.com>
2021-04-17 19:04:03 +0500

    Update settings.py

--- SocialNetwork/SocialNetwork/settings.py
+++ SocialNetwork/SocialNetwork/settings.py
@@ -135,5 +135,5 @@ STATICFILES_DIRS = [
     '/images/users/',
     '/images/posts/',
 ]
-STATIC_ROOT = '/'
+STATIC_ROOT = '/static/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
