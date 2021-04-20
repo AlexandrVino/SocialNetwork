eed7bc88aad20cb56d353804ffd032ffbf232803
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2021-04-20 13:54:19 +0500

    edit requirements

--- SocialNetwork/SocialNetwork/settings.py
+++ SocialNetwork/SocialNetwork/settings.py
@@ -138,4 +138,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = '/static/'
+
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
